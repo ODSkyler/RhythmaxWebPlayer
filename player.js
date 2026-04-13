46d54dbbe7f9e2401765bae2b262ef5b9e0d50b9
Akash <akashpati89@gmail.com>
2026-04-13 13:28:33 +0530

    Update player.js to remove multi-provider support and keep only TIDAL functionality

---
+++ player.js
@@ -0,0 +1,18 @@
+// player.js
+
+// TIDAL functionality code only
+
+// Example of succinct TIDAL functionality
+function playTidalTrack(trackId) {
+    // Logic to play a track from TIDAL
+}
+
+function pauseTidalTrack() {
+    // Logic to pause the TIDAL track
+}
+
+function stopTidalTrack() {
+    // Logic to stop the TIDAL track
+}
+
+// Add your TIDAL-related code here
